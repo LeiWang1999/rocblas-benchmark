2bac0d266352c9f714ea6fd99e959668a9dc1e90
leiwang1999 <leiwang1999@outlook.com>
2024-11-17 04:12:53 +0000

    fix prefix path

---
+++ build/CMakeFiles/rocblas_benchmark.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rocblas_benchmark.dir/rocblas_benchmark.cpp.o"
+  "CMakeFiles/rocblas_benchmark.dir/rocblas_benchmark.cpp.o.d"
+  "rocblas_benchmark"
+  "rocblas_benchmark.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rocblas_benchmark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
